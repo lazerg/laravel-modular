6e10c5414d91f458e1f27e713c0e8a4120efe4c6
lazerg <lazerg2@gmail.com>
2024-07-02 10:15:06 +0500

    wip;

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,12 +1,15 @@
 <?php
 
-/**
- * Get the path to the modules directory.
- *
- * @param string $path
- * @return string
- */
-function modules_path(string $path = ''): string
-{
-    return base_path('modules/' . $path);
+if (!function_exists('modules_path')) {
+    /**
+     * Get the path to the modules directory.
+     *
+     * @param string $path
+     * @return string
+     */
+    function modules_path(string $path = ''): string
+    {
+        return base_path('modules/' . $path);
+    }
 }
+?>
